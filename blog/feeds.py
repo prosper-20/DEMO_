a647935554f22d0439df9ca9e223963b85ec290d
prosper <edwardprosper001@gmail.com>
2022-10-07 13:14:09 +0100

    updates

--- blog/feeds.py
+++ blog/feeds.py
@@ -6,18 +6,18 @@ from .models import Post
 
 class LatestPostsFeed(Feed):
     title = 'My blog'
-    link = reverse_lazy('blog:post_list')
+    link = reverse_lazy('post_list')
     description = 'New posts of my blog.'
 
 
-def items(self):
-    return Post.published.all()[:5]
+    def items(self):
+        return Post.published.all()[:5]
 
-def item_title(self, item):
-    return item.title
+    def item_title(self, item):
+        return item.title
 
-def item_description(self, item):
-    return truncatewords_html(markdown.markdown(item.body), 30)
+    def item_description(self, item):
+        return truncatewords_html(markdown.markdown(item.body), 30)
 
-def item_pubdate(self, item):
-    return item.publish
+    def item_pubdate(self, item):
+        return item.publish
