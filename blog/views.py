e798c66353cd209b456d8bc12048062c55d3e9d5
prosper <edwardprosper001@gmail.com>
2022-10-07 12:05:04 +0100

    updates

--- blog/views.py
+++ blog/views.py
@@ -6,24 +6,26 @@ from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
+from taggit.models import Tag
 
-
-# def post_list(request):
-#     post_list = Post.published.all()
-#     # Pagination with 3 posts per page
-#     paginator = Paginator(post_list, 3)
-#     page_number = request.GET.get('page', 1)
-#     try:
-#         posts = paginator.page(page_number)
-#     except PageNotAnInteger:
-#         # If page_number is not an integer deliver the first page
-#         posts = paginator.page(1)
-#     except EmptyPage:
-#         # If page_number is out of range deliver last page of results
-#         posts = paginator.page(paginator.num_pages)
-#     return render(request,
-#                  'blog/post/list.html',
-#                  {'posts': posts})
+def post_list(request, tag_slug=None):
+    post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request,
+    'blog/post/list.html',
+    {'posts': posts,
+    'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
