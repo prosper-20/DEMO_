990688d8b267588fc98b81fdb6c6670dab2e7e6c
prosper <edwardprosper001@gmail.com>
2022-10-07 12:37:40 +0100

    updates

--- PROJECT/urls.py
+++ PROJECT/urls.py
@@ -18,9 +18,14 @@ from django.urls import path, include
 from django.contrib.sitemaps.views import sitemap
 from blog.sitemaps import PostSitemap
 
+sitemaps = {
+    'posts': PostSitemap,
+}
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
-    path('sitemap.xml', sitemap, {'sitemaps': sitemap}, name='django.contrib.sitemaps.views.sitemap')
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap')
     
 ]
