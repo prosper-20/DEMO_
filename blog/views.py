6642ddcfccfa6db8c0eefcd4d8cb66671b61abda
prosper <edwardprosper001@gmail.com>
2022-10-10 10:43:16 +0100

    updates

--- blog/views.py
+++ blog/views.py
@@ -3,11 +3,12 @@ from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage,\
                                   PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector
 
 def post_list(request, tag_slug=None):
     post_list = Post.published.all()
@@ -107,3 +108,25 @@ def post_comment(request, post_id):
                            {'post': post,
                             'form': form,
                             'comment': comment})
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.published.annotate(
+            search=SearchVector('title', 'body'),
+            ).filter(search=query)
+
+    context = {
+        "form": form,
+        "query": query,
+        "results": results
+    }
+
+    return render(request, 'blog/post_search.html', context)
