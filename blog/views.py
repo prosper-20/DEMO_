551e5d2577d7b5cb32645a45cac3f834062f412c
prosper <edwardprosper001@gmail.com>
2022-10-10 11:00:22 +0100

    updates

--- blog/views.py
+++ blog/views.py
@@ -8,7 +8,8 @@ from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+
 
 def post_list(request, tag_slug=None):
     post_list = Post.published.all()
@@ -119,9 +120,12 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = SearchVector('title', 'body')
+            search_query = SearchQuery(query)
             results = Post.published.annotate(
-            search=SearchVector('title', 'body'),
-            ).filter(search=query)
+            search=search_vector,
+            rank=SearchRank(search_vector, search_query)
+            ).filter(search=search_query).order_by('-rank')
 
     context = {
         "form": form,
